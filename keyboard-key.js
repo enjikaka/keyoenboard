738358b668157da2a448ed1d3c53e9c32f5ab3cf
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2019-05-22 17:29:04 +0200

    Re-use template content between instances.

--- keyboard-key.js
+++ keyboard-key.js
@@ -1,64 +1,72 @@
-const style = document.createElement('style');
-
-style.textContent = `
-  :host {
-    background-color: white;
-    font-family: Helvetica, Arial, sans-serif;
-    min-width: 54px;
-    height: 54px;
-    contain: strict;
-    display: inline-block;
-    position: relative;
-    border-radius: 6px;
-    border: 1px solid black;
-    font-size: 12px;
-    box-sizing: border-box;
-    box-shadow: inset 6px 3px 0 #ccc, inset -6px -6px 0 #ccc, inset 6px -6px 0 #ccc, inset -6px 3px 0 #ccc;
-    transition: box-shadow 50ms ease;
-    user-select: none;
+function getTemplate () {
+  if (document.querySelector('template[data-for="keyboard-keys"]')) {
+    return document.querySelector('template[data-for="keyboard-keys"]').content.cloneNode(true);
   }
 
-  :host(:active) {
-    background-color: #eee;
-    box-shadow: inset 6px 6px 0 #bbb, inset -6px -6px 0 #bbb, inset 6px -6px 0 #bbb, inset -6px 6px 0 #bbb;
-  }
+  const template = document.createElement('template');
 
-  ::slotted(span) {
-    margin: 9px;
-  }
+  template.setAttribute('data-for', 'keyboard-keys');
 
-  ::slotted([slot="shift"]) {
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
+  template.innerHTML = `
+    <style>
+    :host {
+      background-color: white;
+      font-family: Helvetica, Arial, sans-serif;
+      min-width: 54px;
+      height: 54px;
+      contain: strict;
+      display: inline-block;
+      position: relative;
+      border-radius: 6px;
+      border: 1px solid black;
+      font-size: 12px;
+      box-sizing: border-box;
+      box-shadow: inset 6px 3px 0 #ccc, inset -6px -6px 0 #ccc, inset 6px -6px 0 #ccc, inset -6px 3px 0 #ccc;
+      transition: box-shadow 50ms ease;
+      user-select: none;
+    }
 
-  ::slotted([slot="main"]) {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-  }
+    :host(:active) {
+      background-color: #eee;
+      box-shadow: inset 6px 6px 0 #bbb, inset -6px -6px 0 #bbb, inset 6px -6px 0 #bbb, inset -6px 6px 0 #bbb;
+    }
 
-  ::slotted([slot="altgr"]) {
-    position: absolute;
-    bottom: 0;
-    right: 0;
-  }
-`;
+    ::slotted(span) {
+      margin: 9px;
+    }
 
-const template = document.createElement('template')
+    ::slotted([slot="shift"]) {
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
 
-template.innerHTML = `
-  <slot name="shift"></slot>
-  <slot name="main"></slot>
-  <slot name="altgr"></slot>
-`;
+    ::slotted([slot="main"]) {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+    }
+
+    ::slotted([slot="altgr"]) {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+    }
+    </style>
+    <slot name="shift"></slot>
+    <slot name="main"></slot>
+    <slot name="altgr"></slot>
+  `;
+
+  document.body.appendChild(template);
+
+  return template.content.cloneNode(true);
+}
 
 class KeyboardKey extends HTMLElement {
   connectedCallback () {
     this.sDOM = this.attachShadow({ mode: 'closed'});
-    this.sDOM.appendChild(style.cloneNode(true));
-    this.sDOM.appendChild(template.content.cloneNode(true));
+    this.sDOM.appendChild(getTemplate());
 
     const dataWidth = this.getAttribute('data-width');
 
